25d4af8a145189439ba36667401af6ab8c44c81b
Arthur Tolchinsky <arthur.tolchinsky@franklinsquare.com>
2017-12-15 11:13:16 -0500

    feat(mobile): added mobile draft control via socketio

--- src/components/DraftBoard.vue
+++ src/components/DraftBoard.vue
@@ -8,40 +8,56 @@
             <Summary id="draftSummary2" :team="teams[1]" :timerInput="{}"></Summary>
         </div>
     </div>
-        <div class="hidden-sm hidden-md hidden-lg">
-            <button class="btn btn-default col-xs-12">Lager Team</button>
-            <button class="btn btn-default col-xs-12">Porter Team</button>  
+        <div v-if="!teamName" class="hidden-sm hidden-md hidden-lg">
+            <h2>Select your team</h2>
+            <hr>
+            <button class="btn btn-default col-xs-12" @click="teamName = teams[0].name">{{teams[0].name}}</button>
+            <hr>
+            <button class="btn btn-default col-xs-12" @click="teamName = teams[1].name">{{teams[1].name}}</button>
         </div>
+        <div v-if="teamName" class="hidden-sm hidden-md hidden-lg">
+            <h2>{{teamName}}</h2>
+            <hr>
+            <button v-if="teamName === teams[0].name" @click="nextRound(teams[0])" type="button" id="draftNext" class="btn btn-primary btn-lg btn-block xxlFont">Cheers!</button>
+            <button v-if="teamName === teams[1].name" @click="nextRound(teams[1])" type="button" id="draftNext" class="btn btn-primary btn-lg btn-block xxlFont">Cheers!</button>
+
+        </div>
+
     </div>
 </template>
 
 
 <script>
-var STORAGE_KEY = "fs-beer-draft-2017";
+var STORAGE_KEY = 'fs-beer-draft-2017'
 var draftStorage = {
-  fetch: function() {
-    var drafts = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    fetch: function() {
+        var drafts = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
 
-    draftStorage.uid = drafts.length;
-    return drafts;
-  },
-  save: function(drafts) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(drafts));
-  }
-};
+        draftStorage.uid = drafts.length
+        return drafts
+    },
+    save: function(drafts) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(drafts))
+    }
+}
 
-import Summary from "./Summary.vue";
+import Summary from './Summary.vue'
 
 export default {
-  name: "DraftBoard",
-  data() {
-    return {
-      teams: draftStorage.fetch()
-    };
-  },
-  components: {
-    Summary
-  },
-  methods: {}
-};
+    name: 'DraftBoard',
+    data() {
+        return {
+            teams: draftStorage.fetch(),
+            teamName: null
+        }
+    },
+    components: {
+        Summary
+    },
+    methods: {
+        nextRound(team) {
+            this.$socket.emit("next round", team);
+        }
+    }
+}
 </script>
